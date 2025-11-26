fa210346874ae2718dd3ab4cbdf8097a484f5357
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2025-11-26 17:05:25 -0500

    allow multiple lookahead

--- controller.py
+++ controller.py
@@ -7,8 +7,7 @@ from simulator import RaceTrack
 prevErrorDelta = 0.0
 prevErrorV = 0.0
 
-# PARAMETERS
-LOOKAHEAD_DISTANCE = 30
+LOOKAHEAD_DISTANCE_TURN = 30
 
 KpV = 1.0
 KdV = 0.2
@@ -18,19 +17,27 @@ KpDELTA = 4.0
 KdDELTA = 0.5
 KiDELTA = 0.1
 
-def getLookaheadPoint(closest: int, path: np.ndarray):
+def getLookaheadPoints(closest: int, path: np.ndarray, lookahead_distances: list[int] = [30]) -> list[np.ndarray]:
+    """
+    Get raceline points `lookahead_distances` ahead of current position
+    """
     N = path.shape[0]
     i = closest
     dist = 0.0
+    results = []
+    lookahead_index = 0
 
-    while dist < LOOKAHEAD_DISTANCE:
+    while dist < lookahead_distances[-1]:
         j = (i + 1) % N # cyclic
         space = np.linalg.norm(path[i] - path[j])
         dist += space
         i = j
+        while lookahead_index < len(lookahead_distances) and dist >= lookahead_distances[lookahead_index]:
+            results.append(path[i])
+            lookahead_index += 1
         if space == 0: break
 
-    return path[i]
+    return results
 
 def lower_controller(
     state : ArrayLike, # car state: [sx, sy, delta, v, phi]
@@ -75,6 +82,15 @@ def lower_controller(
 
     return np.array([deltaRate, a]).T
 
+def getAlpha(lookAheadPoint: np.ndarray, pos: list[float, float], phi: np.float64):
+    dx = lookAheadPoint[0] - pos[0]
+    dy = lookAheadPoint[1] - pos[1]
+
+    desiredPhi = np.arctan2(dy, dx)
+    headingError = desiredPhi - phi
+
+    return np.arctan2(np.sin(headingError), np.cos(headingError))
+
 def controller(
     state : ArrayLike, # car state: [sx, sy, phi, v, delta]
     parameters : ArrayLike, # car parameters: [l_wb, a_max, v_max, delta_max, etc]
@@ -95,20 +111,16 @@ def controller(
     path = racetrack.centerline
 
     closest = np.argmin(np.linalg.norm(path - pos, axis=1))
-    lookaheadPoint = getLookaheadPoint(closest, path)
+    lookaheadPoints = getLookaheadPoints(closest, path, [LOOKAHEAD_DISTANCE_TURN])
 
-    dx = lookaheadPoint[0] - pos[0]
-    dy = lookaheadPoint[1] - pos[1]
+    alphas = [getAlpha(point, pos, phi) for point in lookaheadPoints]
 
-    desiredPhi = np.arctan2(dy, dx)
-    headingError = desiredPhi - phi
-    alpha = np.arctan2(np.sin(headingError), np.cos(headingError))
-
-    deltaR = np.arctan(2 * wb * np.sin(alpha) / LOOKAHEAD_DISTANCE)
+    deltaR = np.arctan(2 * wb * np.sin(alphas[0]) / LOOKAHEAD_DISTANCE_TURN)
     deltaR = np.clip(deltaR, deltaMin, deltaMax)
 
-    turn_factor = np.abs(alpha)
-    vR = vMax / (1 + 5 * turn_factor)  
+    vR = vMax / (1 + 10 * max([np.abs(alpha) for alpha in alphas]))  
     vR = np.clip(vR, vMin, vMax)
+
+    # print("{:.2f}, {:.2f}, [{:.2f}, {:.2f}], [{:.2f}, {:.2f}]".format(turn_factor, vR, pos[0], pos[1], lookaheadPoint[0], lookaheadPoint[1]))
 
     return np.array([deltaR, vR]).T
